b33c870139bcf06a41b2cf16dc32ab573c2e49f8
Isidro López <islomar@gmail.com>
2019-01-26 22:38:52 +0100

    [quizzes] Format class and add link to README

--- quizzes/README.md
+++ quizzes/README.md
@@ -1,5 +1,5 @@
 # Quizzes
-Oracle quizzes
+Oracle Dev Gym: https://devgym.oracle.com/pls/apex/f?p=10001:200:113442482409969::NO:RP::
    
 ## Prerequisites
 Java 11 
--- quizzes/src/main/java/es/islomar/quizzes/FunctionalInterfaceChallenge.java
+++ quizzes/src/main/java/es/islomar/quizzes/FunctionalInterfaceChallenge.java
@@ -6,16 +6,13 @@ import java.util.function.Function;
 
 public class FunctionalInterfaceChallenge {
 
-  static BiFunction<Integer, Integer, Integer> heisenberg =
-      (a, b) -> a + b;
+  static BiFunction<Integer, Integer, Integer> heisenberg = (a, b) -> a + b;
 
   static Function<Integer, Integer> jesse = a -> a - 2;
 
-  static BiFunction<Integer, Integer, Integer> saul =
-      heisenberg.andThen(jesse);
+  static BiFunction<Integer, Integer, Integer> saul = heisenberg.andThen(jesse);
 
-  static BiConsumer<Integer, Integer> gustavo =
-      (a, b) -> System.out.println(a + 2 / b + 2);
+  static BiConsumer<Integer, Integer> gustavo = (a, b) -> System.out.println(a + 2 / b + 2);
 
   public static void main(String[] breakingBad) {
     var firstLaunder = saul.apply(2, 2);
